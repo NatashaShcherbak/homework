48b7038a5dd1509ee571296db47c73ea762c530a
Natasha Shcherbak <snatasham@ukr.net>
2022-07-11 22:55:51 +0300

    change

--- homework-16/_babel/dist/script.js
+++ homework-16/_babel/dist/script.js
@@ -1,6 +1,10 @@
 "use strict";
 
-function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 function _toArray(arr) { return _arrayWithHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableRest(); }
 
@@ -24,26 +28,5 @@ var data = function data(_ref) {
 
   if (rest.length < 2) console.log('Error');
   console.log("".concat(a, "-").concat(b, "-").concat(c));
-
-  var dataMap = function dataMap() {
-    var map = new Map();
-
-    var _iterator = _createForOfIteratorHelper(rest),
-        _step;
-
-    try {
-      for (_iterator.s(); !(_step = _iterator.n()).done;) {
-        var item = _step.value;
-        map.set(rest.indexOf(item), item);
-      }
-    } catch (err) {
-      _iterator.e(err);
-    } finally {
-      _iterator.f();
-    }
-
-    console.log(Object.fromEntries(map));
-  };
-
-  dataMap();
+  console.log(_objectSpread({}, rest));
 };
--- homework-16/_babel/src/script.js
+++ homework-16/_babel/src/script.js
@@ -1,13 +1,6 @@
 const data = ([a = 'Error',b,c,...rest]) => {
     if (rest.length < 2) console.log('Error');
     console.log(`${a}-${b}-${c}`);
-    const dataMap = () => {
-        const map = new Map();
-        for (let item of rest) {
-            map.set(rest.indexOf(item), item);
-        }
-        console.log(Object.fromEntries(map));
-    }
-    dataMap();
+    console.log({...rest});
 }
 
