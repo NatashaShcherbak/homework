bc522c29590f88eb7413b8f363a606e0283bb632
Natasha Shcherbak <snatasham@ukr.net>
2022-07-30 18:48:50 +0300

    change

--- homework-17/script.js
+++ homework-17/script.js
@@ -1,11 +1,13 @@
 function Form(form) {
     const url = 'https://jsonplaceholder.typicode.com/posts/';
+    const postPlace = document.querySelector('.js--post');
+    const commentsPlace = document.querySelector('.js--comments');
     let idPost = document.querySelector('.js--id');
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        if (idPost.value > 1 && idPost.value < 100 && idPost.value !== '') {
-           return this.post();
+        if (idPost.value >= 1 && idPost.value <= 100 && idPost.value !== '') {
+            return this.post();
         } else {
             alert("Wrong! Let's write a number from 1 to 100");
             form.reset();
@@ -14,18 +16,20 @@ function Form(form) {
 
     this.post = async function() {
         try {
-                const response = await fetch(`${url}${idPost.value}`, {
-                    method: 'GET'
-                })
-                const data = await response.json();
+            const response = await fetch(`${url}${idPost.value}`, {
+                method: 'GET'
+            })
+            const data = await response.json();
+            if (response.status === 200) {
                 const titlePost = data.title;
                 const bodyPost = data.body;
-                document.querySelector('.js--post').innerHTML = `
-                <p class="post__title post__text">${titlePost}</p>
-                <p class="post__text">${bodyPost}</p>`;
+                postPlace.innerHTML = `
+                    <p class="post__title post__text">${titlePost}</p>
+                    <p class="post__text">${bodyPost}</p>`;
                 return this.comments();
+            }
         } catch {
-            document.querySelector('.js--post').innerHTML = 'ERROR';
+            postPlace.innerHTML = 'ERROR';
         }
     }
 
@@ -35,21 +39,24 @@ function Form(form) {
                 method: 'GET'
             })
             const data = await response.json();
-            for (let elem of data) {
-                const name = elem.name;
-                const email = elem.email;
-                const bodyComments = elem.body;
-                const template = `
+            if(response.status === 200) {
+                commentsPlace.innerHTML = '';
+                for (let elem of data) {
+                    const name = elem.name;
+                    const email = elem.email;
+                    const bodyComments = elem.body;
+                    const template = `
                     <li class="comments__item">
                         <p class="comments__text comments__text_uppercase">${name}</p>
                         <p class="comments__text comments__text_orange">${email}</p>
                         <p class="comments__text">${bodyComments}</p>
                     </li>`
-                document.querySelector('.js--comments').insertAdjacentHTML('beforeend', template);
+                    commentsPlace.insertAdjacentHTML('beforeend', template);
+                }
+                form.reset();
             }
-            if(response.status === 200) form.reset();
         } catch {
-            document.querySelector('.js--comments').innerHTML = 'ERROR';
+            commentsPlace.innerHTML = 'ERROR';
         }
     }
 }
